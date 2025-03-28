53b4ef42d43706b0b8ad1303be5a6a3cd6ff42dc
Alyssa Pupping <125580811+ajpupping@users.noreply.github.com>
2025-03-28 00:38:44 +0000

    calculations checked with system print output

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-// COLLABORATORS: <tutors, instructor, or anyone else who helped you>
+// STUDENT NAME: Alyssa Pupping
+// COLLABORATORS: N/A
+// DATE: 3/27/25
 
-// DESCRIPTION: <a sentence about what the purpose of this program is>
+// DESCRIPTION: The purpose of this program is to convert dollar amounts into quarters, dimes, nickels, and pennies.
 
 public class Main
 {
@@ -8,9 +10,37 @@ public class Main
 	{
     //DECLARATION + INITIALIZATION SECTION
 
+    double initialAmount = 1.68;
+    int centsRemaining = 0;
+    int numQuarters = 0;
+    int numDimes = 0;
+    int numNickels = 0;
+    int numPennies = 0;
+  
+
     //CALCULATION SECTION
 
+    centsRemaining = (int) (100 * initialAmount);
+
+    numQuarters = centsRemaining / 25;
+    centsRemaining %= 25;
+
+    numDimes = centsRemaining / 10;
+    centsRemaining %= 10;
+
+    numNickels = centsRemaining / 5;
+    centsRemaining %= 5;
+
+    numPennies = centsRemaining;
+    
+
+
     //OUTPUT SECTION
+    System.out.println("Dollar Amount: $" + initialAmount);
+    System.out.println("Quarters: " + numQuarters);
+    System.out.println("Dimes: " + numDimes);
+    System.out.println("Nickels: " + numNickels);
+    System.out.println("Pennies: " + numPennies);
 
   }
 
